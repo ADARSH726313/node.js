abad2aed6f780420e6eb59783b3469ad4e16c30e
ADARSH726313 <123352620+ADARSH726313@users.noreply.github.com>
2023-07-27 14:44:02 +0530

    Add files via upload

---
+++ import and export of module/app.js
@@ -0,0 +1,21 @@
+
+ function sum (a,b){
+
+    return  a+b ;
+ }
+
+ 
+ function multi (a,b){
+
+    return  a*b ;
+ }
+
+  let obj = {
+
+    name:"adarsh",
+    life :" hope to be beautiful"
+  }
+
+  let name = "adith" ;
+
+  module.exports = { sum ,multi,obj,name}
---
+++ import and export of module/index.js
@@ -0,0 +1,8 @@
+ const { sum ,multi,obj,name} = require("./app")
+
+ console.log( sum(2,3))
+
+ console.log( multi(2,3))
+ console.log( obj)
+ console.log( name)
+
