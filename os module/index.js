4706b19eead94b956a3f778f4c12fe123ef4408c
ADARSH726313 <123352620+ADARSH726313@users.noreply.github.com>
2023-07-27 11:11:37 +0530

    Add files via upload

---
+++ os module/index.js
@@ -0,0 +1,33 @@
+const os = require("os");
+
+// let a =  os.version()
+
+//  let b = os.userInfo()
+
+// console.log(b)
+
+// let c  = os.arch();
+// console.log(c);
+
+
+// let d = os.freemem()/1024/1024/1024;
+// console.log(d);
+
+
+// let e = os.totalmem()/1024/1024/1024 ;
+
+// console.log(e);
+
+// let f = os.hostname();
+// console.log(f)
+
+let g = os.platform();
+console.log(g)
+
+
+
+
+
+
+
+
